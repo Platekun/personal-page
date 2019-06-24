23effd76b35324b2466ecbe8c32492808acb874a
Carlos Camilo <carloscamilo.loboulloque@gmail.com>
2019-06-24 00:48:08 -0500

    👔

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { SEO, Layout, Hero, AboutMe } from '../@features';
+import { SEO, Layout, Hero, AboutMe, WorkExperience } from '../@features';
 
 export default function IndexPage() {
   return (
     <Layout>
       <SEO />
-
       <Hero />
       <AboutMe />
+      <WorkExperience />
     </Layout>
   );
 }
