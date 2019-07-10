646ea3406869e19c2d72152aa07f7ad0c60d9186
Carlos Camilo <carloscamilo.loboulloque@gmail.com>
2019-07-10 13:25:16 -0500

    Migrate SectionTitle to css modules

---
+++ src/@components/@atoms/SectionTitle/SectionTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import clsx from 'clsx';
+
+import styles from './SectionTitle.module.css';
+
+export function SectionTitle({ className, children, ...rest }) {
+  return <h2 className={clsx([styles.sectionTitle, className])}>{children}</h2>;
+}
